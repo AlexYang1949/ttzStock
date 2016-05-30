2092e82b9d8369356d163e5316931279e3f7fe02
alexYang <zhaoheng.yang@zhulebei.com>
2016-05-30 18:20:10 +0800

    修改获取数据方法

--- tushare/src/dailyData/getDataWithKType.py
+++ tushare/src/dailyData/getDataWithKType.py
@@ -2,11 +2,12 @@
 #coding=utf-8
 import tushare as ts
 
-def getStockKInfo(codeArr):
-    df = ts.get_hist_data('399001',start='2016-05-18',ktype='30')
+def getStockKInfo(code):
+    df = ts.get_hist_data(code,start='2016-05-18',ktype='30')
 #     gfb = df[['open','close']]
 #     print(ts.get_index())
     print(df)
 
 if __name__ == '__main__':
-    getStockKInfo(['399001','150206','399973'])
+#     ['399001','150206','399973']
+    getStockKInfo('399001')
