aadde8c18b0e0f53bfe4ba6d66e7f73ad64786e5
alexYang <zhaoheng.yang@zhulebei.com>
2016-06-01 15:38:52 +0800

    定时任务

--- dataSource/__init__.py
+++ dataSource/__init__.py
@@ -4,11 +4,15 @@
 from stockK import stockK
 
 def printInfo():
-    stock = stockK("399001")
+#     stock = stockK("399001")
+    stock = stockK("002273")
 #     68 57
-    print stock.K(ref=1)
-    print stock.D(ref=1)
-    print stock.status(ref=1)
+#     print stock.K(ref=1)
+#     print stock.D(ref=1)
+#     print stock.status(ref=1)
+    stock123 = stock.getHistData()
+    if stock123!=None:
+        print 'OK + %s' % len(stock123)
     
 if __name__ == "__main__":
     printInfo()
