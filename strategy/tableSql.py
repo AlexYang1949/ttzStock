18a1915f33533db7742921881fee171347f32bfc
alexYang <zhaoheng.yang@zhulebei.com>
2017-01-03 19:02:50 +0800

    画图

--- strategy/tableSql.py
+++ strategy/tableSql.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import pymysql
 import tushare as ts
 import pandas as pd
+import matplotlib.pyplot as plt
 import time
 
 from sqlalchemy import Column, String, create_engine
@@ -72,21 +72,29 @@ def test_insert():
         print(time.strftime("%H:%M:%S", time.localtime()))
         # print(df)
         con.close()
+# 存储大盘数据   sh=上证指数 sz=深圳成指 hs300=沪深300指数 sz50=上证50 zxb=中小板 cyb=创业板
+def insert_index_data(code):
+    # 获取数据
+    index_data = ts.get_hist_data(code=code)
+    con = engine.connect()
+    with con:
+        print(code+'开始存储')
+        index_data['code']=code
+        index_data.to_sql(name='%s_data'%code,con=con,if_exists='replace',index=True,index_label='date')
+        print(code+'存储成功')
 
-def test_all_stock_info():
-    # 获取所有的股票
+# 每日股票数据存储
+def insert_all_stock_info():
+    # 所有的股票
     today_all = ts.get_today_all()
     # print(today_all)
     with con:
-        # .drop(['name'], axis=1)
-        # today_all['name'].apply(lambda name: name.encode('utf-8').decode('utf-8'))
-        # print(today_all['name'])
         today_all.to_sql(name='today_all',con=con,if_exists='append',index=True,index_label='num')
         df = pd.read_sql('select * from today_all;', con=con)
         print(df)
         # print(df.index,df.columns)
 
-def test_stock_info(code):
+def insert_stock_info(code):
     # 获取股票
     stock_info = ts.get_hist_data(code=code)
     if stock_info is None:
@@ -97,7 +105,6 @@ def test_stock_info(code):
         stock_info['code']=code
         stock_info.to_sql(name='stock_info',con=con,if_exists='append',index=True)
         print(code+'存储成功')
-        # print(df.index,df.columns)
 
 def get_table_data(name):
     with con:
@@ -105,7 +112,7 @@ def get_table_data(name):
         df = pd.read_sql('select * from %s WHERE num>152;'%name, con=con)
         # df = pd.read_sql('select * from %s where code=603188;'%name, con=con)
         for code in df['code']:
-            test_stock_info(code)
+            insert_stock_info(code)
 
 
 def test_zz500s():
@@ -123,6 +130,12 @@ if __name__ == '__main__':
     # print(df)
     # test_all_stock_info()
     # test_stock_info('600345')
-    get_table_data('today_all')
+    # get_table_data('today_all')
     # df = pd.read_sql('select * from stock_info;' , con=con)
     # print(df)
+    # insert_index_data('sh')
+    df = pd.read_sql('select close,volume from sh_data;', con=con)
+    df['volume'] = df['volume'].apply(lambda v:v/1500)
+    print(df)
+    df.plot(kind='line')
+    plt.show()
